50badedbd7f4ab048016867c36c6268a3fd8d163
jsh-1235 <>
2022-01-12 18:08:25 +0900

    update

--- public/study_dream_coding/js/script3.js
+++ public/study_dream_coding/js/script3.js
@@ -11,3 +11,24 @@
 // }
 
 console.log("page 3", sum(100));
+
+Object.assign({}, {});
+
+class Student {
+  constructor(name, age, enrolled, score) {
+    this.name = name;
+    this.age = age;
+    this.enrolled = enrolled;
+    this.score = score;
+  }
+}
+
+const students = [new Student("A", 29, true, 45), new Student("B", 28, false, 80), new Student("C", 30, true, 90), new Student("D", 40, false, 66), new Student("E", 18, true, 88)];
+
+function func14() {
+  const result = students.find((item) => {
+    if (item.score === 90);
+  });
+
+  console.log(result);
+}
