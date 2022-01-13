21d7d366ea1cc58aebcf07abaa8d8ed6fa812553
jsh-1235 <>
2022-01-13 16:09:59 +0900

    update

--- public/study_dream_coding/js/script3.js
+++ public/study_dream_coding/js/script3.js
@@ -23,12 +23,4 @@ class Student {
   }
 }
 
-const students = [new Student("A", 29, true, 45), new Student("B", 28, false, 80), new Student("C", 30, true, 90), new Student("D", 40, false, 66), new Student("E", 18, true, 88)];
-
-function func14() {
-  const result = students.find((item) => {
-    if (item.score === 90);
-  });
-
-  console.log(result);
-}
+console.log();
